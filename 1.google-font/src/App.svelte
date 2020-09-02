651d6131aea394a519fc703f0f3f3d1eccae3b19
neulhan <neulhan@lawandgood.com>
2020-09-02 19:36:58 +0900

    1.국을폰트 클론

---
+++ 1.google-font/src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import FontBox from "./font-box.svelte";
+  let text = "그들의 장비와 기구는 모두 살아 있다.";
+  let size = 32;
+
+  const fontList = [
+    "Noto Sans KR",
+    "Nanum Myeongjo",
+    "Nanum Gothic",
+    "Gugi",
+    "Jua",
+    "Do Hyeon",
+    "Black Han Sans",
+    "Yeon Sung",
+  ];
+</script>
+
+<style>
+  :global(.page) {
+    width: 100%;
+    min-height: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  :global(.container) {
+    width: 1300px;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+  .top {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .top > input {
+    box-sizing: border-box;
+    padding: 12px;
+    width: calc(50% - 10px);
+    border-radius: 8px;
+  }
+  .center {
+    margin-top: 100px;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
+
+<div class="page">
+  <div class="container">
+    <h1>국을 폰트</h1>
+    <div class="top">
+      <input
+        placeholder="테스트 문자를 입력하세요"
+        type="text"
+        bind:value={text} />
+      <input placeholder="폰트 사이즈" type="number" bind:value={size} />
+    </div>
+    <div class="center">
+      {#each fontList as font}
+        <FontBox {text} {font} {size} />
+      {/each}
+    </div>
+  </div>
+</div>
---
+++ 1.google-font/src/font-box.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let font;
+  export let text;
+  export let size;
+</script>
+
+<style>
+  .font-box {
+    width: 300px;
+    background: #fff;
+    border-radius: 8px;
+    border: 1px solid #dadce0;
+    box-shadow: 0px 0px 0px #fff, 0px 0px 0px #fff, 0px 0px 0px #fff;
+    box-sizing: border-box;
+    padding: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    transition: 0.18s ease all;
+  }
+  .font-box:hover {
+    transform: scale(1.05);
+  }
+  :global(.font-box:nth-child(4n)) {
+    margin-right: 0;
+  }
+</style>
+
+<div
+  class="font-box"
+  style="font-family: '{font}', sans-serif; font-size: {size}px">
+  {text}
+</div>
